34e2213323804a52e4185a67aab38101d0d5a01d
Daniel Stern <daniel@danielstern.ca>
2018-09-23 11:41:01 -0400

    :piano:

---
+++ README.md
@@ -0,0 +1,11 @@
+# [WIP] Nord Lead A1 Controller
+
+## NodeJS Nord Lead A1 Library
+
+This library is a work in progress.
+
+### Features [ Beta ]
+#### Nord Lead A1 Parameters
+#####`lib/ControlCode.json` 
+**[Nord Lead A1 Parameters]** can be used for any application capable of loading JSON. Contents: all Nord Lead A1 Control Codes corresponding to available parameters. 
+
--- index.spec.js
+++ index.spec.js
@@ -14,47 +14,59 @@ describe("Nord AI Tests",()=>{
         // await delay(250);
     });
     //
-    // it("Play a scale",async ()=>{
-    //     let noteIndex = 35;
-    //     for (let i = 0; i <= 0xf; i++) {
-    //         transmit(Message.NOTE_ON(noteIndex + i, 0x7f, 0x00), start + i * s);
-    //         transmit(Message.NOTE_OFF(noteIndex + i, 0x7f, 0x00), start + i * s + s);
-    //     }
-    //     await delay(s * 0xf);
-    // });
-    //
-    // it("Program select",async()=>{
-    //     for (let i = 0; i < 10; i++) {
-    //         transmit(Message.PROGRAM_CHANGE(i), start + i*s*2);
-    //         transmit(Message.NOTE_ON(0x30, 0x7f, 0x00), start + i*s*2 + s);
-    //         transmit(Message.NOTE_OFF(0x30, 0x7f, 0x00), start + i*s*2 + s*2);
-    //     }
-    //     await delay(s * 0xf);
-    // });
-    //
-    // it("Bank select ",async()=>{
-    //     for (let i = 0; i < 8; i++) {
-    //         await transmit(Message.BANK_SELECT_MSB(0x00));
-    //         await transmit(Message.BANK_SELECT_LSB(i));
-    //         await transmit(Message.PROGRAM_CHANGE(0x00));
-    //         await delay(s);
-    //         await transmit(Message.NOTE_ON(0x30, 0x7f, 0x00));
-    //         await transmit(Message.NOTE_OFF(0x30, 0x7f, 0x00), window.performance.now() + s * 0x04);
-    //         await delay(s * 0x04);
-    //     }
-    // })
-    //
-    it("Arp Run",async ()=>{
-        await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 127),start);
-        await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 0),start + 1000);
-        await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 127),start + 2000);
-        await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 0),start + 3000);
-        for (let note of [0x30,0x34,0x38]) {
-            await transmit(Message.NOTE_ON(note, 0x7f, 0x00));
-            await transmit(Message.NOTE_OFF(note, 0x7f, 0x00), start + 4000);
+    it("Play a scale",async ()=>{
+        let noteIndex = 35;
+        for (let i = 0; i <= 0xf; i++) {
+            transmit(Message.NOTE_ON(noteIndex + i, 0x7f, 0x00), start + i * s);
+            transmit(Message.NOTE_OFF(noteIndex + i, 0x7f, 0x00), start + i * s + s);
         }
-        await delay(4000);
+        await delay(s * 0xf);
+    });
+
+    it("Program select",async()=>{
+        for (let i = 0; i < 10; i++) {
+            transmit(Message.PROGRAM_CHANGE(i), start + i*s*2);
+            transmit(Message.NOTE_ON(0x30, 0x7f, 0x00), start + i*s*2 + s);
+            transmit(Message.NOTE_OFF(0x30, 0x7f, 0x00), start + i*s*2 + s*2);
+        }
+        await delay(s * 0xf);
+    });
+
+    it("Bank select ",async()=>{
+        for (let i = 0; i < 8; i++) {
+            await transmit(Message.BANK_SELECT_MSB(0x00));
+            await transmit(Message.BANK_SELECT_LSB(i));
+            await transmit(Message.PROGRAM_CHANGE(0x00));
+            await delay(s);
+            await transmit(Message.NOTE_ON(0x30, 0x7f, 0x00));
+            await transmit(Message.NOTE_OFF(0x30, 0x7f, 0x00), window.performance.now() + s * 0x04);
+            await delay(s * 0x04);
+        }
     })
+    //
+    describe("Arpeggiator",()=>{
+        it("Arp Run",async ()=>{
+            await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 127),start);
+            for (let note of [0x30,0x34,0x38]) {
+                await transmit(Message.NOTE_ON(note, 0x7f, 0x00));
+                await transmit(Message.NOTE_OFF(note, 0x7f, 0x00), start + 4000);
+            }
+            await delay(4000);
+        })
+
+        it("Arp tempo",async()=>{
+            await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_RUN, 127),start);
+            for (let i = 0; i < 0x08; i++) {
+                await transmit(Message.CONTROL_CHANGE(ControlCode.ARP_TEMPO__CLK_DIVISIONS, 127 - 0x0F * i),start + 200 * i);
+            }
+            for (let note of [0x30,0x34,0x38]) {
+                await transmit(Message.NOTE_ON(note, 0x7f, 0x00));
+                await transmit(Message.NOTE_OFF(note, 0x7f, 0x00), start + 4000);
+            }
+            await delay(4000);
+        });
+    });
+
 
 
 });
